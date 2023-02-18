be9352e60af1e34cb17bc9a34f6ff5a433a0e45d
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-18 18:30:14 +0500

    setted up tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const App = () => {
-  return <div>App</div>
+  return <div className="w-screen h-screen flex items-center justify-center text-blue-600">App</div>
 }
 
 export default App
