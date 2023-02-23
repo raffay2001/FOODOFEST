789447d5605a12ebbcbcbf2298fa66a26f51161b
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-23 11:42:22 +0500

    add: header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react"
+import Logo from "./img/logo.png"
 
 const Header = () => {
   return (
-    <div className="z-50 w-screen bg-slate-300 p-6 px-16">
+    <div className="z-50 w-screen p-6 px-16">
       {/* For desktop and tablets  */}
-      <div className="hidden md:flex w-full h-full bg-red-600 p-4"></div>
+      <div className="hidden md:flex w-full h-full p-4">
+        <img src={Logo} alt="logo" className="w-8 object-cover" />
+        <p className="text-headingColor text-xl font-bold">City</p>
+      </div>
 
       {/* For mobile phones  */}
-      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
+      <div className="flex md:hidden w-full h-full0 p-4"></div>
     </div>
   )
 }
