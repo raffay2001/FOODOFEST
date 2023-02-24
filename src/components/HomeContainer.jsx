67f6e376ac773dbf2aa04a48a7f2b678fd9c3023
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-24 20:00:44 +0500

    add: setted up createItem container

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -5,7 +5,7 @@ import { HERO_DATA } from "../utils/constants"
 
 const HomeContainer = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full my-8" id="home">
       {/* Hero Section Description */}
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
         {/* Delivery Button */}
