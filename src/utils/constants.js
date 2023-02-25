b4d496f1b02d970ce9c63f8f5885a6d27b7e7869
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-25 10:21:56 +0500

    add: started CreateContainer component

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,3 +33,42 @@ export const HERO_DATA = [
     imageSrc: Fi1,
   },
 ]
+
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+]
