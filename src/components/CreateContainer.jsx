468588a6eab7cd61bfa6e2c587de2416109a3126
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-25 11:03:14 +0500

    add: Create Container UI done

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react"
 import { motion } from "framer-motion"
-import { MdFastfood } from "react-icons/md"
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from "react-icons/md"
+import { CATEGORIES } from "../utils/constants"
+import Loader from "./Loader"
 
 const CreateContainer = () => {
+  // reactive state variables for controlled input components.
   const [title, setTitle] = useState("")
   const [calories, setCalories] = useState("")
   const [price, setPrice] = useState("")
@@ -12,9 +15,18 @@ const CreateContainer = () => {
   const [alertStatus, setAlertStatus] = useState("danger")
   const [msg, setMsg] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+
+  // handler function for uploading the image to firebase database.
+  const uploadImage = () => {}
+  // handler function for deleting the image from UI.
+  const deleteImage = () => {}
+  // handler function for submitting the details to backend.
+  const saveDetails = () => {}
+
   return (
     <div className="w-full min-h-screen flex items-center justify-center">
-      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+      <div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
+        {/* Error Handling JSX */}
         {fields && (
           <motion.p
             initial={{ opacity: 0 }}
@@ -29,6 +41,7 @@ const CreateContainer = () => {
             {msg}
           </motion.p>
         )}
+        {/* Title Input */}
         <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
           <MdFastfood className="text-xl text-gray-700" />
           <input
@@ -37,8 +50,109 @@ const CreateContainer = () => {
             required
             placeholder="Give me a title..."
             onChange={(e) => setTitle(e.target.value)}
-            className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+            className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
           />
+        </div>
+        {/* Category Input */}
+        <div className="w-full">
+          <select
+            onChange={(e) => setCategory(e.target.value)}
+            className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+          >
+            <option value="other" className="bg-white">
+              Select Category
+            </option>
+            {CATEGORIES.map((categoryItem) => (
+              <option
+                key={categoryItem.id}
+                className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+                value={categoryItem.urlParamName}
+              >
+                {categoryItem.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* Image Input */}
+        <div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {!imageAsset ? (
+                <>
+                  <label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+                    <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+                      <MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+                      <p className="text-gray-500">Click here to upload</p>
+                    </div>
+                    <input
+                      type="file"
+                      name="uploadimage"
+                      accept="image/*"
+                      onChange={uploadImage}
+                      className="w-0 h-0"
+                    />
+                  </label>
+                </>
+              ) : (
+                <>
+                  <div className="relative h-full">
+                    <img
+                      src={imageAsset}
+                      alt="uploadedImg"
+                      className="w-full h-full object-cover"
+                    />
+                    <button
+                      type="button"
+                      className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
+                      onClick={deleteImage}
+                    >
+                      <MdDelete className="text-white" />
+                    </button>
+                  </div>
+                </>
+              )}
+            </>
+          )}
+        </div>
+        {/* Calories Input */}
+        <div className="w-full flex flex-col md:flex-row items-center gap-3">
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdFoodBank className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              placeholder="Calories"
+              value={calories}
+              onChange={(e) => setCalories(e.target.value)}
+              className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+        </div>
+        {/* Price Input */}
+        <div className="w-full flex flex-col md:flex-row items-center gap-3">
+          <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+            <MdAttachMoney className="text-gray-700 text-2xl" />
+            <input
+              type="text"
+              required
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400 text-textColor"
+            />
+          </div>
+        </div>
+        {/* Submit Button */}
+        <div className="flex items-center w-full">
+          <button
+            type="button"
+            className="ml-0 md:ml-auto w-full md:w-auto border-none outline-none text-white bg-emerald-500 px-12 py-2 rounded-lg text-lg text-center font-semibold"
+            onClick={saveDetails}
+          >
+            Save
+          </button>
         </div>
       </div>
     </div>
