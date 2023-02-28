8b452e36cb261c18762d5f80611228b07011d314
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-02-28 10:13:11 +0500

    add: updated firebase config file with env vars

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAl0dYEc8CLozaQcN36b0LHI8No-VMbY4o",
-  authDomain: "restaurantapp-8b570.firebaseapp.com",
-  databaseURL: "https://restaurantapp-8b570-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-8b570",
-  storageBucket: "restaurantapp-8b570.appspot.com",
-  messagingSenderId: "376557735375",
-  appId: "1:376557735375:web:5cca4c0d033fa22c77d6ba",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig)
